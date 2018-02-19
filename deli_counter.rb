4af7a0464422af1660885bd138d4bbd9f98f67f4
Jay Fleming <jacobmfleming@gmail.com>
2018-02-19 13:23:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+
+katz_deli = []
+
+
+def line
+  if katz_deli.length == 0
+    return "The line is currently empty."
+  else
+    return katz_deli
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+
+end
