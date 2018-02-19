9a2393b9a4e2a1b3932206ea0b10ae0496fe1a04
Jay Fleming <jacobmfleming@gmail.com>
2018-02-19 13:23:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,11 @@
 katz_deli = []
 
 
-def line
-  if katz_deli.length == 0
+def line(array)
+  if array.length == 0
     return "The line is currently empty."
   else
-    return katz_deli
-  end
+    return array
 end
 
 
